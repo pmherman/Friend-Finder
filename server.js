b6338e76370fb0503e24d8baf9380fad046a9eeb
Paul M Herman <>
2018-02-27 20:46:41 -0500

    no message

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 //Setup for local CSS Use
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 //Routes
 require("./app/routing/apiRoutes")(app);
